870dd050623e8d18d7ce0e09bd99b4978ae0e046
Tommy Susilo <36617814+tommysusilo@users.noreply.github.com>
2024-11-14 22:23:10 +0800

    Initial commit

---
+++ src/aplikasi_cek_nomor_genap_ganjil/Aplikasi_cek_nomor_genap_ganjil.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasi_cek_nomor_genap_ganjil;
+
+/**
+ *
+ * @author Win 10
+ */
+public class Aplikasi_cek_nomor_genap_ganjil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
